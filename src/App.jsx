4db4e9ea8d24a9c804e4f24f19237b32fdf77775
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2024-06-07 11:09:32 +0200

    simplify style

--- src/App.jsx
+++ src/App.jsx
@@ -29,14 +29,13 @@ function App() {
 	}
 
 	return (
-		<div className="container">
-			<Input onAddTask={AddTask}></Input>
-			<List tasks={tasks} removeTask={removeTask}></List>
-			<img src={ToDoTextImg} alt="Description" className="img-text"></img>
-			<a href="https://github.com/v-dav" target="_blank" rel="noopener noreferrer">
-				<img src={GitHub} alt="Description" className="img-github"></img>
-			</a>
-		</div>
+		<>
+			<div className="container">
+				<Input onAddTask={AddTask}></Input>
+				<List tasks={tasks} removeTask={removeTask}></List>
+				<img src={ToDoTextImg} alt="Description" className="img-text"></img>
+			</div>
+		</>
 	)
 }
 
