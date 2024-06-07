c452c6d82872bd24033350c02783f4228b1fc158
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2024-06-07 10:21:22 +0200

    fix syntax typos

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -5,8 +5,8 @@ const Item = ({ task, removeTask }) => {
 		<div className='item'>
 			<p>{task}</p>
 			<button className='remove' onClick={() => removeTask(task)}>
-				<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="none" viewBox="-3 -5 30 30" stroke="currentColor" stroke-width="2">
-					<path stroke-linecap="round" stroke-linejoin="round" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
+				<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="none" viewBox="-3 -5 30 30" stroke="currentColor" strokeWidth="2">
+					<path strokeLinecap="round" strokeLinejoin="round" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
 				</svg>
 			</button>
 		</div>
